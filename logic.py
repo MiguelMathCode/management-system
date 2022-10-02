f95b03818e26ce2a9ed74df307f63e6b04f406ae
Miguel Math Code <mike23507@gmail.com>
2022-10-01 21:28:10 -0500

    modyfing logic.py

--- logic.py
+++ logic.py
@@ -12,9 +12,17 @@ class Bookings:
     cellphone: str = None
     date: np.datetime64 = None
     payment_mehtod: str = None
-    status: str = None
+    status: str = "Pending for approval"
 
 
-book1 = Bookings("Miguel", 2, 1, "+ 507 6223 0196", np.datetime64("2022-10-02"), "cash", "Waiting")
+name = input("Enter name of booking: ")
+nights = int(input("Enter amount of nights you want to stay: "))
+beds = int(input("Enter amount of beds you want to your stay: "))
+cellphone = input("Please enter your phone number: ")
+date = np.datetime64(input("Enter date of your stay: "))
+payment_mehtod = input("Enter your payment method: ")
 
-print(f"{book1.name} wants to stay {book1.nights} with us. He asked for {book1.beds} beds for his stay, his phone is {book1.cellphone}, he wants to stay on {book1.date}, he is going to pay {book1.payment_mehtod}, status of booking {book1.status}")
+book = Bookings(name, nights, beds, cellphone, date, payment_mehtod)
+
+
+print(book.name, book.nights, book.cellphone, book.date, book.payment_mehtod, book.status)
