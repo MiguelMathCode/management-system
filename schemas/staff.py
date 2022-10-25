d6b266982621c32b30842df90406a13c918dad5c
Miguel Math Code <mike23507@gmail.com>
2022-10-24 21:50:10 -0500

    working implementation of pub/sub dev

--- schemas/staff.py
+++ schemas/staff.py
@@ -1,15 +1,19 @@
-"""This script defines the Staff of the hotel
- Make use of composition in order to reduce repetition"""
+"""
+This script defines the Staff of the hotel
+Make use of composition in order to reduce repetition
+"""
 
 
 from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Optional, List
 from dataclasses import dataclass
 
 
 @dataclass
 class Employee():
-    """Defining the class Employee"""
+    """
+    Defining the class Employee
+    """
     name: str = None
     surname: str = None
     employee_id: str = None
